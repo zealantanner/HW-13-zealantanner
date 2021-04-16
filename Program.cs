8a925655f6070cfa41509b7c7e6d99349eb48962
zealantanner <77339919+zealantanner@users.noreply.github.com>
2021-04-16 17:08:52 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public readonly struct Fraction
+{
+    private readonly int num, den;
+
+    public Fraction(int numerator, int denominator)
+    {
+        num = numerator;
+        den = denominator;
+    }
+
+    public static Fraction operator +(Fraction a, Fraction b)
+        => new Fraction(a.num * b.den + b.num * a.den, a.den * b.den);
+
+    public static Fraction operator -(Fraction a, Fraction b)
+        => new Fraction(a.num * b.den - b.num * a.den, a.den * b.den);
+
+    public static Fraction operator *(Fraction a, Fraction b)
+        => new Fraction(a.num * b.num, a.den * b.den);
+
+    public static Fraction operator /(Fraction a, Fraction b)
+        => new Fraction(a.num * b.den, a.den * b.num);
+
+
+    public static Fraction operator %(Fraction a, Fraction b)
+        => new Fraction((a.num * b.den) % (b.num * a.den), a.den * b.den);
+
+
+    public override string ToString() => $"{num} / {den}";
+}
+
+public static class OperatorOverloading
+{
+    public static void Main()
+    {
+        var a = new Fraction(5, 4);
+        var b = new Fraction(2, 3);
+        var c = new Fraction(5, 7);
+        Console.WriteLine(a + b);
+        Console.WriteLine(c - a);
+        Console.WriteLine(b * c);
+        Console.WriteLine(a / b);
+
+        Console.WriteLine(a % b);
+    }
+}
